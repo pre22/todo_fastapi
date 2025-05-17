094e86bf1b0e3ae9ba5d2a2219d837307702504f
Precious Chika <43546663+pre22@users.noreply.github.com>
2025-05-17 18:54:24 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
 from app.routes import auth as auth_routes
+from middleware import log_middleware
 
 app = FastAPI()
 
+# Register router
 app.include_router(auth_routes.router)
+# Register Middleware
+app.middleware("http")(log_middleware)
 
 
 if __name__ == "__main__":
