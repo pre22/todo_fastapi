b704b67cfbb58d4b552c265514732e70e876a810
Precious Chika <43546663+pre22@users.noreply.github.com>
2025-05-17 16:51:16 +0100

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from auth import routes as auth_routes
+
+app = FastAPI()
+
+app.include_router(auth_routes.router)
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", reload=True)
