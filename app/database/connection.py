712a7d5bbf04dec065cee566f8352ff4b8f5244c
Precious Chika <43546663+pre22@users.noreply.github.com>
2025-05-31 12:57:06 +0100

    refactored sqlmodel to sqlachemy connection pattern

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,12 +1,19 @@
-from sqlmodel import Session, create_engine, SQLModel
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, DeclarativeBase
 
 DATABASE_URL  = "sqlite:///./todo.db"
 
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(
+    DATABASE_URL, 
+    echo=True
+)
 
-def get_session():
-    with Session(engine) as session:
-        yield session
-    
-def init_db():
-    SQLModel.metadata.create_all(engine)
+connection = engine.connect()
+
+
+class Base(DeclarativeBase):
+    pass
+
+# pool_size: How many active DB connections should SQLAlchemy keep available at all times?
+# max_overflow: Number of extra temporary connections allowed beyond pool_size
+# echo=True: SQLAlchemy will print all SQL statements it sends to the database.
