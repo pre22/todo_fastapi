0477f57569ca65d9c56db594bbaa0510fe6598b2
Precious Chika <43546663+pre22@users.noreply.github.com>
2025-05-31 13:31:18 +0100

    refactored Todo tablet using sqlachemy

--- app/models/todos.py
+++ app/models/todos.py
@@ -1,9 +1,18 @@
-import uuid
-from sqlmodel import SQLModel, Field
+from uuid import UUID, uuid4
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from database.connection import Base
+from models.users import User
+
+class Todo(Base):
+    id: Mapped[UUID] = mapped_column(
+        primary_key=True,
+        insert_default=uuid4,
+        index=True
+    )
+    title: Mapped[str]
+    completed: Mapped[bool]
+    user_id: Mapped[UUID] = mapped_column(ForeignKey("user_account.id"))
+    user: Mapped[User] = relationship(back_populates="todos")
 
 
-class Todo(SQLModel, table=True):
-    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True, index=True)
-    title: str
-    completed: bool = False
-    owner_id: int
