7ad2d992a6d14df542968c437902293f5598f6ed
Precious Chika <43546663+pre22@users.noreply.github.com>
2025-05-31 12:56:28 +0100

    created user and address model in sqlachemy style

--- app/models/users.py
+++ app/models/users.py
@@ -1,12 +1,33 @@
-import uuid
-from sqlmodel import SQLModel, Field
+from uuid import UUID, uuid4
+from typing import List, Optional
+from sqlalchemy import ForeignKey, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from database.connection import Base
 
 
-class User(SQLModel, table=True):
-    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True, index=True)
-    username: str
-    email: str
-    hashed_password: str    
+class User(Base):
+    __tablename__ = "user_account"
 
+    id: Mapped[UUID] = mapped_column(primary_key=True, insert_default=uuid4, init=False)
+    name: Mapped[str] = mapped_column(String(30))
+    username: Mapped[Optional[str] | None]
+
+    addresses = Mapped[List["Address"]] = relationship(back_populates="user")
+
+    def __repr__(self):
+        return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
+
+
+class Address(Base):
+    __tablename__ = "address"
+
+    id: Mapped[UUID] = mapped_column(primary_key=True, insert_default=uuid4, init=False)
+    email_address: Mapped[str]
+    user_id: Mapped[User] = mapped_column(ForeignKey("user_account.id"))
+    user: Mapped[User] = relationship(back_populates="addresses")
+
+    def __repr__(self):
+        return f"Address(id={self.id!r}, email_address={self.email_address!r})"
 
 
