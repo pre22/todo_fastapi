412485e361d165d0021810bd99f012cb830b2b6c
Precious Chika <43546663+pre22@users.noreply.github.com>
2025-06-01 08:15:15 +0100

    worked on todo apis

--- app/routes/todos.py
+++ app/routes/todos.py
@@ -1,17 +1,83 @@
-from fastapi import APIRouter, Depends
-from schemas.todo import TodoCreate, TodoOut
-from models.todos import Todo
+from uuid import UUID
+from typing import List
+
+from fastapi import APIRouter, Depends, HTTPException
+
+from schemas.todo import TodoCreate, Todo, TodoUpdate
+from schemas.users import UserCreate
+from crud.todo import create_todo, get_todo, get_todos, update_todo as ut, delete_todo as dt
+
+from auth.dependencies import get_current_active_user
+from database.connection import get_db
+
+from models import todos as tmodels
 from models.users import User
-from database.connection import get_session
-from sqlmodel import Session
-from auth.protected import get_current_user
 
 router = APIRouter()
 
-@router.post("/create", response_model=TodoOut)
-def create_todo(todo: TodoCreate, username: str = Depends(get_current_user), session: Session = Depends(get_session)):
-    db_todo = Todo(title=todo.title, owner_id=1)
-    session.add(db_todo)
-    session.commit()
-    session.refresh(db_todo)
-    return db_todo
+@router.post("/", response_model=Todo)
+def create_todo(
+    todo: TodoCreate,
+    db=Depends(get_db),
+    current_user: User = Depends(get_current_active_user),
+):
+    return create_todo(db=db, todo=todo, user_id=current_user.id)
+
+
+@router.get("/", response_model=List[Todo])
+def read_todos(
+    skip: int = 0,
+    limit: int = 100,
+    db=Depends(get_db),
+    current_user: User = Depends(get_current_active_user),
+):
+    todos = get_todos(db, skip=skip, limit=limit)
+    return todos
+
+
+@router.get("/{todo_id}", response_model=Todo)
+def read_todo(
+    todo_id: UUID,
+    db=Depends(get_db),
+    current_user: User = Depends(get_current_active_user),
+):
+    todo = get_todo(db, todo_id=todo_id)
+    if todo is None:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    
+    if todo.user_id != current_user.id:
+        raise HTTPException(status_code=400, detail="Access Denied")
+    
+    return todo
+
+@router.put("/{todo_id}", response_model=Todo)
+def update_todo(
+    todo_id: UUID,
+    todo: TodoUpdate,
+    db=Depends(get_db),
+    current_user: User = Depends(get_current_active_user),
+):
+    db_todo = get_todo(db, todo_id=todo_id)
+    if db_todo is None:
+        raise HTTPException(status_code=404, details="Todo not found")
+    
+    if db_todo.user_id != current_user.id:
+        raise HTTPException(status_code=400, detail="Not Enough Permissions")
+    
+    return ut(db=db, todo_id=todo_id, todo=todo)
+
+@router.delete("/{todo_id}", response_model=Todo)
+def delete_todo(
+    todo_id: UUID,
+    db=Depends(get_db),
+    current_user: User = Depends(get_current_active_user),
+):
+    db_todo = get_todo(db, todo_id=todo_id)
+
+    if db_todo is None:
+        raise HTTPException(status_code=404, detail="Todo not Found")
+    
+    if db_todo.user_id != current_user.id:
+        raise HTTPException(status_code=400, detail="Not Enough Permission")
+    
+    return dt(db=db, todo_id=todo_id)
