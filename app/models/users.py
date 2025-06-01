c5582e515e4a6e5197ac0ee3421eede4889c882c
Precious Chika <43546663+pre22@users.noreply.github.com>
2025-06-01 09:22:33 +0100

    updated methods, added field to user model

--- app/models/users.py
+++ app/models/users.py
@@ -12,24 +12,25 @@ class User(Base):
 
     id: Mapped[UUID] = mapped_column(primary_key=True, insert_default=uuid4, init=False)
     name: Mapped[str] = mapped_column(String(30))
-    username: Mapped[Optional[str] | None]
+    email: Mapped[Optional[str] | None]
+    hashed_password: Mapped[str]
 
     addresses = Mapped[List["Address"]] = relationship(back_populates="user")
     todos = Mapped[List["Todo"]] = relationship(back_populates="user", cascode="all, delete-orphan")
 
     def __repr__(self):
-        return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
+        return f"User(id={self.id!r}, name={self.name!r}, email={self.email!r})"
 
 
 class Address(Base):
     __tablename__ = "address"
 
     id: Mapped[UUID] = mapped_column(primary_key=True, insert_default=uuid4, init=False)
-    email_address: Mapped[str]
+    address: Mapped[str]
     user_id: Mapped[User] = mapped_column(ForeignKey("user_account.id"))
     user: Mapped[User] = relationship(back_populates="addresses")
 
     def __repr__(self):
-        return f"Address(id={self.id!r}, email_address={self.email_address!r})"
+        return f"Address(id={self.id!r}, address={self.address!r})"
 
 
