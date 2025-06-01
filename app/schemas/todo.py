a2efc3a81f9ab245c3eb8a60fbca5eb75383a63c
Precious Chika <43546663+pre22@users.noreply.github.com>
2025-06-01 06:29:26 +0100

    updated todo schema

--- app/schemas/todo.py
+++ app/schemas/todo.py
@@ -1,11 +1,28 @@
+# The schema is used to filter and validate data from and to your tables respectively
+from uuid import UUID
 from pydantic import BaseModel
+from typing import Optional
 
 
-class TodoCreate(BaseModel):
+class TodoBase(BaseModel):
     title: str
+    description: Optional[str] = None
 
 
-class TodoOut(BaseModel):
-    id: int
-    title: str
+class TodoCreate(TodoBase):
+    pass
+
+
+class TodoUpdate(BaseModel):
+    title: Optional[str] = None
+    description: Optional[str] = None
+    completed: Optional[bool] = None
+
+
+class Todo(TodoBase):
+    id: UUID
     completed: bool
+    user_id: UUID
+
+    class Config:
+        orm_mode = True
