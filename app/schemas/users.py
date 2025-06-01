d453af253c06c0b403e97bc05274643150629b01
Precious Chika <43546663+pre22@users.noreply.github.com>
2025-06-01 06:32:37 +0100

    update user schema

--- app/schemas/users.py
+++ app/schemas/users.py
@@ -1,7 +1,25 @@
-from pydantic import BaseModel
+from uuid import UUID
+from typing import Optional
+from pydantic import BaseModel, EmailStr
 
 
-class UserCreate(BaseModel):
-    username: str
-    email: str
+class UserBase(BaseModel):
+    email: EmailStr
+
+
+class UserCreate(UserBase):
     password: str
+
+
+class UserUpdate(BaseModel):
+    email: Optional[EmailStr] = None
+    password: Optional[EmailStr] = None
+
+
+class User(UserBase):
+    id: UUID
+    is_active: bool
+
+
+    class Config:
+        orm_mode = True
