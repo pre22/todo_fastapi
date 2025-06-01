212643d05085388732d7e9318ff6ecf328b817b6
Precious Chika <43546663+pre22@users.noreply.github.com>
2025-06-01 10:24:41 +0100

    refactored crud

--- app/crud/todo.py
+++ app/crud/todo.py
@@ -1,5 +1,6 @@
 from uuid import UUID
 from sqlalchemy import select, update, delete
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload, Session
 
 from models.todos import Todo
@@ -8,33 +9,42 @@ from models.users import User
 from schemas import todo as todo_schemas
 
 
-async def get_todo(db: Session, todo_id: UUID):
-    return db.query(Todo).filter(Todo.id == todo_id).first()
+async def get_todo(db, todo_id: UUID):
+    result = await db.execute(
+        select(Todo)
+        .where(Todo.id == todo_id)
+    )
+    return result.scalar_one_or_none()
 
-def get_todos(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(Todo).offset(skip).limit(limit).all()
+async def get_todos(db, skip: int = 0, limit: int = 100):
+    result = await db.execute(
+        select(Todo)
+        .offset(skip)
+    )
+    return result.scalars().all()
 
-def create_todo(db: Session, todo: todo_schemas.TodoCreate, user_id: UUID):
-    db_todo = Todo(**todo.dict(), user_id=user_id)
+async def create_todo(db, todo: todo_schemas.TodoCreate, user_id: UUID):
+    db_todo = Todo(**todo.model_dump(), user_id=user_id)
     db.add(db_todo)
-    db.commit()
-    db.refresh(db_todo)
+    await db.commit()
+    await db.refresh(db_todo)
     return db_todo
 
-def update_todo(db: Session, todo_id: UUID, todo: todo_schemas.TodoUpdate):
-    db_todo = get_todo(db, todo_id)
-    if db_todo is None:
-        return None
-    for field, value in todo.dict(exclude_unset=True).items():
-        setattr(db_todo, field, value)
-    db.commit()
-    db.refresh(db_todo)
-    return db_todo
+async def update_todo(db, todo_id: UUID, todo: todo_schemas.TodoUpdate):
+    result = await db.execute(
+        update(Todo)
+        .where(Todo.id == todo_id)
+        .values(**todo.model_dump(exclude_unset=True))
+        .returning(Todo)
+    )
+    await db.commit()
+    return result.scalar_one()
 
-def delete_todo(db: Session, todo_id: UUID):
-    db_todo = get_todo(db, todo_id)
-    if db_todo is None:
-        return None
-    db.delete(db_todo)
-    db.commit()
-    return db_todo
+async def delete_todo(db, todo_id: UUID):
+    result = await db.execute(
+        delete(Todo)
+        .where(Todo.id == todo_id)
+        .returning(Todo)
+    )
+    await db.commit()
+    return result.scalar_one()
