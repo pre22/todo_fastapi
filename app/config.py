e36383329d3a8d7edd33c2fc502055a90fbc3eb7
Precious Chika <43546663+pre22@users.noreply.github.com>
2025-06-01 21:50:44 +0100

    fixed error, conflicting env fetch

--- app/config.py
+++ app/config.py
@@ -1,15 +1,16 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 from decouple import config
 
 
 class Settings(BaseSettings):
     app_name: str = "Todo API"
     admin_email: str
-    database_url: str =  config("database_url")
-    async_database_url: str = config("async_database_url")
+    database_url: str 
+    async_database_url: str 
     secret_key: str
     algorithm: str = "HS256"
     access_token_expire_minutes: int = 30
+    debug: bool
 
     class Config:
         env_file = ".env"
