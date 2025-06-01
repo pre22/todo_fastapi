5da327518002a4b64550cd7fbc53deedc1a13163
Precious Chika <43546663+pre22@users.noreply.github.com>
2025-06-01 21:52:13 +0100

    updated target_metadata

--- alembic/env.py
+++ alembic/env.py
@@ -2,11 +2,10 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
-
-from app.models.todos import SQLModel, Todo
-from app.models.users import User
+from app.database.connection import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -21,7 +20,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = SQLModel.metadata
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
